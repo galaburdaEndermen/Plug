2c35e402638fdad0b4bfe9bb349ee44765e436a8
Anton Halaburda <a.halaburda@brainence.com>
2022-06-15 22:44:02 +0300

    test

--- placeholder.js
+++ placeholder.js
@@ -15,37 +15,39 @@ define(function (require) {
 
 
 
-            // let divs = document.getElementsByClassName("dialog ProcessedOrders_ReturnsView")?.[0];
-            let divs = document.getElementsByTagName("div");
+            // // let divs = document.getElementsByClassName("dialog ProcessedOrders_ReturnsView")?.[0];
+            // let divs = document.getElementsByTagName("div");
 
-            for (var form of divs) {
-                if (form.className.includes("ProcessedOrders_ReturnsView")) {
-                    let plus = form.getElementsByClassName("fa fa-plus-square-o fa-lg  slick-icon-row-toggle")?.[0];
-                    if (plus) {
-                        plus.click();
+            // for (var form of divs) {
+            //     if (form.className.includes("ProcessedOrders_ReturnsView")) {
+            //         let plus = form.getElementsByClassName("fa fa-plus-square-o fa-lg  slick-icon-row-toggle")?.[0];
+            //         if (plus) {
+            //             plus.click();
 
-                        var cell = document.getElementsByClassName("slick-cell l5 r5")?.[0];
-                        if (cell) {
-                            var inner = cell.getElementsByTagName("span")?.[0];
-                            let reference = inner.innerHTML;
-                        }
-                    }
-                }
-            }
+            //             var cell = document.getElementsByClassName("slick-cell l5 r5")?.[0];
+            //             if (cell) {
+            //                 var inner = cell.getElementsByTagName("span")?.[0];
+            //                 let reference = inner.innerHTML;
+            //             }
+            //         }
+            //     }
+            // }
+
+
             // if (form) {
 
             // }
 
-            // let script = "select fkOrderId as Id from [Order_Refund] where [RefundReference] = '07D668DEE509CB'";
-            // let parameters = [];
-            // Services.executeRequestBySession({}, `/api/Dashboards/ExecuteCustomScriptWithParameters`, { script, parameters }, event => {
-            //     if (event.result.IsError) {
-            //         let lal = event.result;
-            //     }
-            //     else {
-            //         let kek = event.result.Results;
-            //     }
-            // });
+            let script = "select CAST(CONVERT(CHAR(16), dReceievedDate,20) AS datetime) from [order] where source = 'DIRECT' and subSource = 'US DEV Shopify' and CAST(CONVERT(CHAR(16), dReceievedDate,20) AS datetime) = CAST('07 Jun 2022 15:04' AS DATETIME)";
+            let parameters = [];
+            Services.executeRequestBySession({}, `/api/Dashboards/ExecuteCustomScriptWithParameters`, { script, parameters }, event => {
+                if (event.result.IsError) {
+                    let lal = event.result;
+                }
+                else {
+                    let kek = event.result.Results;
+                }
+            });
 
 
         }
