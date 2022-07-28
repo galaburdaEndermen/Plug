ca9fa2f124410f3edc042f05ec39c03240dd4a25
Anton Halaburda <a.halaburda@brainence.com>
2022-07-28 16:07:05 +0300

    test errors

--- placeholder.js
+++ placeholder.js
@@ -2,38 +2,54 @@
 
 define(function (require) {
     $(document).ready(function ($scope) {
-        const config = { childList: true, subtree: true };
+        try {
+            const config = { childList: true, subtree: true };
 
-        var callback = function (mutationsList, observer) {
-
-            if (!userPermissions.canSeeTheApp) {
-                var appsContainer = document.getElementsByClassName("cdk-overlay-container")[0];
-                if (appsContainer) {
-                    var moduleContainers = appsContainer.getElementsByClassName("moduleContainer");
-                    if (moduleContainers.length > 0) {
-                        for (var moduleContainer of moduleContainers) {
-                            var nameModule = moduleContainer.getElementsByClassName("module-name-text")[0];
-                            if (nameModule) {
-                                if (nameModule.getAttribute("title") === "Custom Permissions Setup") {
-                                    moduleContainer.innerHTML = "";
-                                    break;
+            var callback = function (mutationsList, observer) {
+                try {
+                    console.log("BACKORDER SCREEN CALLBACK TEST");
+                    if (!userPermissions.canSeeTheApp) {
+                        var appsContainer = document.getElementsByClassName("cdk-overlay-container")[0];
+                        if (appsContainer) {
+                            var moduleContainers = appsContainer.getElementsByClassName("moduleContainer");
+                            if (moduleContainers.length > 0) {
+                                for (var moduleContainer of moduleContainers) {
+                                    var nameModule = moduleContainer.getElementsByClassName("module-name-text")[0];
+                                    if (nameModule) {
+                                        if (nameModule.getAttribute("title") === "Custom Permissions Setup") {
+                                            moduleContainer.innerHTML = "";
+                                            break;
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                } catch (error) {
+                    console.log("BACKORDER SCREEN ERROR");
+                    console.log(error);
                 }
+
             }
+            const observer = new MutationObserver(callback);
+
+            const session = JSON.parse(window.localStorage.getItem("SPA_auth_session"));
+
+            const userPermissions = JSON.parse(getUserPermissions(session.email, session.userName));
+
+            setTimeout(function () {
+                try {
+                    const targetNode = document.getElementsByTagName("body")[0];
+                    observer.observe(targetNode, config);
+                } catch (error) {
+                    console.log("BACKORDER SCREEN ERROR");
+                    console.log(error);
+                }
+            }, 2000);
+        } catch (error) {
+            console.log("BACKORDER SCREEN ERROR");
+            console.log(error);
         }
-        const observer = new MutationObserver(callback);
-
-        const session = JSON.parse(window.localStorage.getItem("SPA_auth_session"));
-
-        const userPermissions = JSON.parse(getUserPermissions(session.email, session.userName));
-
-        setTimeout(function () {
-            const targetNode = document.getElementsByTagName("body")[0];
-            observer.observe(targetNode, config);
-        }, 2000);
     });
 
     function getUserPermissions(ownerEmail, userEmail) {
