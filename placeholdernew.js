cd70227ef596fd7289339d780031f52922e910a6
Anton Halaburda <a.halaburda@brainence.com>
2022-09-02 13:40:16 +0300

    t

--- placeholdernew.js
+++ placeholdernew.js
@@ -3,23 +3,23 @@
 define(function (require) {
     $(document).ready(function ($scope) {
 
-        // const placeholderManager = require("core/placeholderManager");
-        // const ngComponent = require("core/ngComponent");
+        const placeholderManager = require("core/placeholderManager");
+        const ngComponent = require("core/ngComponent");
 
-        // const macroService = new Services.MacroService(self);
+        const macroService = new Services.MacroService(self);
 
-        // var obj = { applicationName: 'Print Invoices', macroName: 'Print_Invoices_Macro', orderIds: ["b2a6554a-db14-43cf-a2f0-8ea5030c0f01", "fbce3128-5961-440a-bf2f-9bba54354fc4", "5f514423-be6a-48aa-8eba-d0b5e9ecfa71"] };
-        // macroService.Run(obj, function (data) {
-        //     if ((data.error == null) && (data.result != null) && (data.result.length != 0)) {
-        //         var orders = data.result;
+        var obj = { applicationName: 'Print Invoices', macroName: 'Print_Invoices_Macro', orderIds: ["b2a6554a-db14-43cf-a2f0-8ea5030c0f01", "fbce3128-5961-440a-bf2f-9bba54354fc4", "5f514423-be6a-48aa-8eba-d0b5e9ecfa71"] };
+        macroService.Run(obj, function (data) {
+            if ((data.error == null) && (data.result != null) && (data.result.length != 0)) {
+                var orders = data.result;
 
-        //         $scope.formInvoice(orders);
-        //         //Finally, create a file.
-        //         pdfMake.createPdf(docDefinition).open();
-        //     } else {
-        //         alert('Errors...');
-        //     }
-        // });
+                $scope.formInvoice(orders);
+                //Finally, create a file.
+                pdfMake.createPdf(docDefinition).open();
+            } else {
+                alert('Errors...');
+            }
+        });
 
         const config = { childList: true, subtree: true };
 
